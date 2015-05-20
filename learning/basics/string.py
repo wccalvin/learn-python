e8f36ea62a2a72c926802e89058e725e5d86054c
wccalvin <clayton.calvin@gmail.com>
2015-05-20 08:00:01 -0400

    Python learning practice

---
+++ learning/basics/string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Empty String
+x = ''
+print x
+
+# String search method
+s = "This is fun"
+find = s.find('fu')
+print find # finds the index of the search string
+
+# String replace method
+print "Before replacing: ", s
+replace = s.replace('fun', 'cool!')
+print "After replacing: ", replace
+
