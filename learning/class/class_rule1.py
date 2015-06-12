302ee24d9aa4b3e2b3953df66507dc36b8e8ddf9
wccalvin <clayton.calvin@gmail.com>
2015-06-12 14:20:32 -0400

    Add class learning files

---
+++ learning/class/class_rule1.py
@@ -0,0 +1,14 @@
+# An instance of a class knows what class its from.
+
+class MyClass(object):
+	pass
+
+# Creating two instances to prove the theory that they are independant!
+
+MyInst1 = MyClass()
+
+print MyInst1
+
+MyInst2 = MyClass()
+
+print MyInst2
---
+++ learning/class/class_rule2.py
@@ -0,0 +1,10 @@
+# Variables defined in the class are available to the instance.
+
+class MyClass(object):
+	var = "Class variable is available!"
+
+first_obj = MyClass()
+print first_obj.var
+
+second_obj = MyClass()
+print second_obj.var
