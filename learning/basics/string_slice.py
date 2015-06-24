bef4931d2b7da25a947a60e0a32ad108434b211c
wccalvin <clayton.calvin@gmail.com>
2015-06-24 13:03:23 -0400

    string slice

---
+++ learning/basics/string_slice.py
@@ -0,0 +1,13 @@
+"""slice: The operator [n:m] returns the part of the string from the n-eth character to the m-eth character.
+   i.e. including the n and excluding the m."""
+
+x = "my string"
+
+"""
+    "my string"
+     |||||||||
+     012345678
+"""
+slice = x[3:6] # inclues the s(3) but excludes the i(6)
+
+print slice
