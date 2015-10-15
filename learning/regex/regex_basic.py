f5a9c36ed759e8235cdba3d7bb2fb497d814b190
Clayton William <clayton.calvin@gmail.com>
2015-10-15 16:28:29 -0400

    just a little more organized

---
+++ learning/regex/regex_basic.py
@@ -0,0 +1,10 @@
+__author__ = 'wccalvin'
+
+import re
+
+file_obj = open('example.txt', 'r')
+
+for line in file_obj:
+    line = line.strip()
+    if re.search(r'^X.*', line):  # ^X -> start with X; . -> any character; * -> any number of times
+        print(line)
