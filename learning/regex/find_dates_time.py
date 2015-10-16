6a6051e5529afce41a49f5886f7fc86f3186f21f
Clayton William <clayton.calvin@gmail.com>
2015-10-16 14:23:04 -0400

    one more example for regex

---
+++ learning/regex/find_dates_time.py
@@ -0,0 +1,12 @@
+__author__ = 'wccalvin'
+
+import re
+
+file_obj = open('date_time_values.txt', 'r')
+
+for line in file_obj:
+    line = line.strip()
+    if re.search(r'.*:', line):  # Match anything with : on the line
+        print(line)              # We can also do it without the .* in it.
+
+file_obj.close()
--- learning/regex/regex_basic.py
+++ learning/regex/regex_basic.py
@@ -8,3 +8,7 @@ for line in file_obj:
     line = line.strip()
     if re.search(r'^X.*', line):  # ^X -> start with X; . -> any character; * -> any number of times
         print(line)
+
+file_obj.close()
+
+regex_basic.py
