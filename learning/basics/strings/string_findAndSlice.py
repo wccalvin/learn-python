d6a36503c076b8fef3ba090d870d8bae7598d778
Clayton William <clayton.calvin@gmail.com>
2016-03-13 19:48:17 -0400

    fix the bug introduced previously

--- learning/basics/strings/string_findAndSlice.py
+++ learning/basics/strings/string_findAndSlice.py
@@ -5,7 +5,7 @@ my_emailText = 'From clayton.calvin@gmail.com Tue Mar 6'
 # Get only the domain names
 
 startIndex = my_emailText.find('@')
-endIndex = my_emailText.find(' ', startIndex)  # read find syntax
-emailDomain = my_emailText[startIndex+1:endIndex+1]
+endIndex = my_emailText.find(' ', startIndex)  # read find documentation
+emailDomain = my_emailText[startIndex+1:endIndex]
 
 print('Email Domain: {}'.format(emailDomain))
