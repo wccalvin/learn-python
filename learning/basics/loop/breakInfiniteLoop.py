8d78017ddd5b0abb138987a50f1eb058aeeae50d
Clayton William <clayton.calvin@gmail.com>
2016-05-15 00:27:16 -0400

    more consistent to pep8 way of writing python

--- learning/basics/loop/breakInfiniteLoop.py
+++ learning/basics/loop/breakInfiniteLoop.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 
-######################################################################
-# 
-# Description: break statement breaks out of the while loop clause 
-# early
-# 
-######################################################################
+"""
+Description: break statement breaks out of the while loop early
+"""
+
 
 number = 0
 while True:
     number += 1
     if number == 100:
         print("Counted to {}!".format(number))
-	break
-
+        break
