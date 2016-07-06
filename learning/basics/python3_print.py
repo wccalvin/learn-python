bf46673c1df240f9a51c97a393ca2ece2ef01cb3
Clayton William <clayton.calvin@gmail.com>
2016-07-06 09:40:35 +0530

    new examples for demostration

---
+++ learning/basics/python3_print.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# To use python3 print in python2 add the below line
+from __future__ import print_function
+
+print("python3 -- Hello World!")
+
+# python2 print will now throw SyntaxError
---
+++ learning/basics/translate_using_replace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Using replace to translate few alphabets to numbers
+"""
+
+import sys
+
+try:
+    my_input = raw_input("Enter some text: ")
+except KeyboardInterrupt:
+    print("User aborted.")
+    sys.exit(1)
+
+rp_a = my_input.replace('a', str(4))
+rp_b = rp_a.replace('b', str(8))
+rp_e = rp_b.replace('e', str(3))
+rp_l = rp_e.replace('l', str(1))
+rp_o = rp_l.replace('o', str(0))
+rp_s = rp_o.replace('s', str(5))
+rp_t = rp_s.replace('t', str(7))
+
+print(rp_t)
