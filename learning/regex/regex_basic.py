14c7216633d8bab6f61bb4aaa2d3a5426dfad6ef
Clayton William <clayton.calvin@gmail.com>
2016-07-09 22:27:31 +0530

    some edits for consistency

--- learning/regex/regex_basic.py
+++ learning/regex/regex_basic.py
@@ -1,14 +1,11 @@
-__author__ = 'wccalvin'
-
 import re
 
 file_obj = open('example.txt', 'r')
 
 for line in file_obj:
     line = line.strip()
-    if re.search(r'^X.*', line):  # ^X -> start with X; . -> any character; * -> any number of times
+    # ^X -> start with X; . -> any character; * -> any number of times
+    if re.search(r'^X.*', line):
         print(line)
 
 file_obj.close()
-
-regex_basic.py
