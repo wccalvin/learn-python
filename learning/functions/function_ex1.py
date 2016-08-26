f7bfb46f8aa011b661a526a34f44c8dbfe0d00fa
Clayton William <clayton.calvin@gmail.com>
2016-08-26 08:09:43 +0530

    make it more generic

--- learning/functions/function_ex1.py
+++ learning/functions/function_ex1.py
@@ -2,21 +2,22 @@
 
 
 def addition(num1, num2):
-    """prints the sum of two numbers."""
-    print num1 + num2
+    """returns the sum of two numbers."""
+    return num1 + num2
 
 
 def difference(num1, num2):
-    """prints the difference between two numbers."""
-    print num1 - num2
+    """returns the difference between two numbers."""
+    return num1 - num2
 
 
 def square(num):
-    """prints the square of given number"""
-    print num**2
+    """returns the square of given number"""
+    return num**2
 
 
 if __name__ == '__main__':
-    addition(20, 5)
-    difference(10, 5)
-    square(5)
+    print(addition(20, 5))
+    print(difference(10, 5))
+    print(square(5))
+    print(addition(2016, 34) + difference(575, 324) - square(15))
