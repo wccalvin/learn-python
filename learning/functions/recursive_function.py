6e718ea50ad0372e573f947665dd9605b6c90b67
Clayton William <clayton.calvin@gmail.com>
2016-11-12 07:37:53 -0500

    few modification to recursive example.

--- learning/functions/recursive_function.py
+++ learning/functions/recursive_function.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
 
+import sys
 
-def myFunction(num):
-    if num == 0:
+"""
+Example depicts a recursive funtion. The function which has a potential to call
+itself.
+"""
+
+
+def rfunc(num):
+    if num < 0:
+        print("Can't use a negative value.")
+    elif num == 0:
         return True
     else:
         print(num)
-        myFunction(num=num - 1)
+        rfunc(num-1)
 
-myFunction(10)
+
+if __name__ == '__main__':
+    try:
+        number = input("Enter an integer: ")
+        try:
+            number = int(number)
+            rfunc(number)
+        except ValueError:
+            print("You've entered a string.")
+            print("Try again with an integer.")
+            sys.exit(1)
+    except NameError:
+        print("Not an integer. Try again.")
+        sys.exit(1)
