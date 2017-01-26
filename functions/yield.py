ef137d722a90c54deb4ea01fb193adb91a49a256
Clayton William <clayton.calvin@gmail.com>
2017-01-25 20:08:44 -0500

    add example for yield

---
+++ functions/yield.py
@@ -0,0 +1,12 @@
+#/usr/local/bin/python
+
+
+def square(num):
+    for i in range(num):
+        yield i**2
+
+if __name__ == "__main__":
+    gen_obj = square(10)
+    print(gen_obj)
+    data = [i for i in gen_obj]
+    print(data)
