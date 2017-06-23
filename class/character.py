5b7f29ac2d5286d1b821589ba124e064e4aba292
Clayton William <clayton.calvin@gmail.com>
2017-06-22 21:44:22 -0400

    using pycharm

--- class/character.py
+++ class/character.py
@@ -1,29 +1,27 @@
 class Character(object):
-	experience = 0
-	hit_points = 10
+    experience = 0
+    hit_points = 10
 
-	def get_weapon(self):
-		weapon_choice = raw_input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
+    def get_weapon(self):
+        weapon_choice = raw_input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
 
-		if weapon_choice in 'sab':
-			if weapon_choice == 's':
-				return "sword"
-			if weapon_choice == 'a':
-				return "axe"
-			if weapon_choice == 'b':
-				return "bow"
-		else:
-			self.get_weapon()
+        if weapon_choice in 'sab':
+            if weapon_choice == 's':
+                return "sword"
+            if weapon_choice == 'a':
+                return "axe"
+            if weapon_choice == 'b':
+                return "bow"
+        else:
+            self.get_weapon()
 
+    # To update the values the below method is used
+    def __init__(self, **kwargs):
+        self.name = raw_input("Name: ")
+        self.weapon = self.get_weapon()
 
-	# To update the values the below method is used
-	def __init__(self, **kwargs):
-		self.name = raw_input("Name: ")
-		self.weapon = self.get_weapon()
-
-		for key, value in kwargs.items():
-			setattr(self, key, value)
-
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 player = Character()
