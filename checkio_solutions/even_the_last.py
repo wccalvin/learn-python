b5af1fa8a81edce2609b2394c62bf41270294834
Clayton William <clayton.calvin@gmail.com>
2017-06-24 23:14:11 -0400

    fix formatting

--- checkio_solutions/even_the_last.py
+++ checkio_solutions/even_the_last.py
@@ -10,9 +10,10 @@ def checkio(array):
         return sum_even * array[-1]
     return 0
 
-# These "asserts" using only for self-checking and not necessary for
-# auto-testing
+
 if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
     assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
     assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
     assert checkio([6]) == 36, "(6)*6=36"
