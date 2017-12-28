665e52dfd72fb3aafb53d23518e313d47ebd9f01
Clayton William <clayton.calvin@gmail.com>
2017-12-28 00:27:21 -0500

    refactor to better version

--- class/game_score_example.py
+++ class/game_score_example.py
@@ -3,12 +3,11 @@
 
 class Game(object):
 
-    def __init__(self):
+    def __init__(self, player):
         self.current_score = [0, 0]
-
-    def score(self, player):
         self.player = player
 
+    def score(self):
         if self.player == 1:
             increment = self.current_score[0] + 1
             self.current_score[0] = increment
@@ -18,5 +17,5 @@ class Game(object):
             self.current_score[1] = increment
             return self.current_score
 
-play = Game()
-print play.score(1)
+play = Game(1)
+print play.score()
